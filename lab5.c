fee9b95cef0566b2972f5c86ea3ddba111154e26
ljitdohn <1287344886@qq.com>
2017-02-17 12:09:18 -0500

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// incomplete c file
+
 int main();
 
 int main(){
@@ -12,6 +14,13 @@ int main(){
     float price;
     
   };
+  FILE *fp;
+  
+  fp = fopen("book_dat.txt", "a+");
+  if(fp == NULL){
+      perror("Error opening file");
+      return(-1);
+    }
   
   struct data rec[40];
   
