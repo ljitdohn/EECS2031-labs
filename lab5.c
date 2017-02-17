ab0d7faa74dbfd0806788a9d5251ac1224596028
ljitdohn <1287344886@qq.com>
2017-02-17 12:04:12 -0500

    Create lab5.c

---
+++ lab5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main();
+
+int main(){
+  
+  struct data{
+    char title[60];
+    char author[50];
+    char publisher[50];
+    float price;
+    
+  };
+  
+  struct data rec[40];
+  
+  
+  
+  
+  
+  
+  
+  return 0;
+}
